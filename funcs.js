368d6f8807307daa7f7e747b3f5ebef911f44170
FlexGames <79639219+FlexGamesGitHub@users.noreply.github.com>
2022-01-27 20:02:41 +0100

    Großes Website Update

--- funcs.js
+++ funcs.js
@@ -1,9 +1,1 @@
-document.getElementById('d-l-s').addEventListener('click', () => {
-    if (document.body.classList.contains('dark-mode')) {
-        document.body.classList.replace('dark-mode', 'light-mode');
-        document.getElementById('d-l-s').innerHTML = "&#9790";
-    } else {
-        document.body.classList.replace('light-mode', 'dark-mode');
-        document.getElementById('d-l-s').innerHTML = "&#9728;";
-    } 
-});
+document.getElementById("d-l-s").addEventListener("click",()=>{document.body.classList.contains("dark-mode")?(document.body.classList.replace("dark-mode","light-mode"),document.getElementById("d-l-s").innerHTML="&#9790"):(document.body.classList.replace("light-mode","dark-mode"),document.getElementById("d-l-s").innerHTML="&#9728;")}),window.onload=function(){window.scrollTo(window.location.hash.slice(1))};
